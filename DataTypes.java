085ccf3fe45fe80cc21247f82e74c07e6e775184
miftahhadi817 <ajago6028@gmail.com>
2021-06-06 17:27:41 +0100

    Output dengan sout

---
+++ DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes{
+ public static void main(String[] args) {
+
+ Scanner cin = new Scanner(System.in);
+ String namaDepan = "Miftah";
+ String namaTengah = "Hadi";
+ String namaBelakang = "Syah";
+ int usia = 20;
+ int targetTahunKuliah = 4;
+ double ipk = 3.89764512;
+ char nilaiAbjad = 'A';
+ boolean tampan = true;
+
+ System.out.println("========== OUTPUT ============");
+ System.out.println("Nama depan : " + namaDepan);
+ System.out.println("Nama tengah : " + namaTengah);
+ System.out.println("Nama belakang: " + namaBelakang);
+ System.out.println("Usia : " + usia);
+ System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
+ System.out.println("IPK : " + ipk);
+ System.out.println("Nilai PBO : " + nilaiAbjad);
+ System.err.println("Tampan : " + tampan);
+ }
+}
